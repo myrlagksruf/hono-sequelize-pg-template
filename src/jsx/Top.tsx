9fb0444666d93e7c2f4a810a1b9832ad2bef2689
myrlagksruf <myrlagksruf@gmail.com>
2023-12-28 14:20:51 +0900

    Top.tsx 수정

--- src/jsx/Top.tsx
+++ src/jsx/Top.tsx
@@ -1,16 +1,25 @@
 import type { FC } from "hono/jsx"
 
-const Layout:FC<{ children?: JSX.Element[]|JSX.Element }> = ({children}) => {
+const Layout:FC<{ children?: JSX.Element[]|JSX.Element, head?: JSX.Element[]|JSX.Element }> = ({children, head}) => {
     return (
-        <html>
-            <body>{children}</body>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8" />
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="viewport" content="width=device-width" />
+                {head}
+            </head>
+            <body>
+                {children}
+            </body>
         </html>
+
     )
 }
 
 export const Top:FC<{ messages: string[] }> = ({messages}) => {
     return (
-        <Layout>
+        <Layout head={<title>탑</title>}>
             <h1>Hello Hono!</h1>
             <ul>
                 {messages.map((message) => {
